198be457894146ac54cd83e7f5d7de33fef93857
codawise-onyekachi <onyekachihopeuzoma@gmail.com>
2025-05-16 20:33:02 +0100

    server and db commit update

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 
+const express = require("express")
+
+const dotenv = require("dotenv").config()
+
+const mongoose = require("mongoose")
+
+const jwt = require("jsonwebtoken")
+
+const bcrypt = require("bcryptjs")
+
+const app = express()
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 1010
+
+mongoose.connect(process.env.MONGODB_URL)
+.then( () => {
+    console.log("MongoDB Connected")
+
+    app.listen(PORT, () => {
+    console.log(`Successful, server is running on port ${PORT}`)
+})
+
+
+})
+
+
+
